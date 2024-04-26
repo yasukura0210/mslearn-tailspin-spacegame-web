6cc8b40166e6c981e87a202fa78ebbe83ae4fa36
yasukura0210 <167849100+yasukura0210@users.noreply.github.com>
2024-04-26 14:31:28 +0900

    Update DocumentDBRepository_GetItemsAsyncShould.cs

--- Tailspin.SpaceGame.Web.Tests/DocumentDBRepository_GetItemsAsyncShould.cs
+++ Tailspin.SpaceGame.Web.Tests/DocumentDBRepository_GetItemsAsyncShould.cs
@@ -48,7 +48,8 @@ namespace Tests
             IEnumerable<Score> scores = scoresTask.Result;
 
             // Verify that each score's game region matches the provided game region.
-            Assert.That(scores, Is.All.Matches<Score>(score => score.GameRegion == gameRegion));
+            // Assert.That(scores, Is.All.Matches<Score>(score => score.GameRegion == gameRegion));
+            Assert.That(scores, Is.All.Matches<Score>(score => score.GameRegion == "Test1"));
         }
     }
 }
